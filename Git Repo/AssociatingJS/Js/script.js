df7529c8bb4a4037d8cbdd7f3f9a51d39a9dd65f
Shelby West <sjwest@fullsail.edu>
2015-03-05 17:59:52 -0500

    Learning String and Boolean Variables

--- Git Repo/AssociatingJS/Js/script.js
+++ Git Repo/AssociatingJS/Js/script.js
@@ -6,3 +6,9 @@ age = 19; //definition
 //storage container = value
 
 var yearOfBirth = 1995; //declaring and defining on the same line
+
+var isStudent = true;
+
+var firstName = "Shelby";
+
+var phrase = "I don't know";
