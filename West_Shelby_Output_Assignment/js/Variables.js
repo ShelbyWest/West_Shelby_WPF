ab696784637d6440854da4f5c557a2c15837e11b
Shelby West <sjwest@fullsail.edu>
2015-03-05 18:58:10 -0500

    Added the Remaining Variables
    Added the number and string variables along with the comments required.

--- West_Shelby_Output_Assignment/js/Variables.js
+++ West_Shelby_Output_Assignment/js/Variables.js
@@ -7,3 +7,8 @@
 
 var languageFluency = true; //Assigned a Boolean value
 var wrestling = true; //Assigned a second Boolean value
+var anyPets = 5; //Assigned number variable
+var favoriteMusic = "Bangtan Boys"; //Assigned string variable
+var olderSiblings = "Maria and Danielle"; //Assigned second string variable
+
+
