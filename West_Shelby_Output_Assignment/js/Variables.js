3744e8164453478e2bcb29f91a743d0e0807ef1d
Shelby West <sjwest@fullsail.edu>
2015-03-05 19:18:30 -0500

    Fixed Grammatical Errors
    Noticed a few grammar issues and fixed those.

--- West_Shelby_Output_Assignment/js/Variables.js
+++ West_Shelby_Output_Assignment/js/Variables.js
@@ -13,6 +13,6 @@ var olderSiblings = "Maria and Danielle"; //Assigned second string variable
 
 console.log("It is " + languageFluency + " that I am fluent in Korean."); //Adding and concatenating the first Boolean variable
 console.log("It is also " + wrestling + " that I have been a life long wrestling fan."); //Added and concatenated the second Boolean variable
-console.log("I have " + anyPets+ " dogs, all Boxers"); //Logging and concatenating the number variable
-console.log("My favorite band is a Korean band called " + favoriteMusic + "."); //Logging and concatenating the first string variable
-console.log("I have two older sisters named, " + olderSiblings + "."); //Added and concatenated the second string variable
+console.log("I have " + anyPets+ " dogs that are all Boxers"); //Logging and concatenating the number variable
+console.log("My favorite band is a Korean band named " + favoriteMusic + "."); //Logging and concatenating the first string variable
+console.log("I have two older sisters named " + olderSiblings + "."); //Added and concatenated the second string variable
