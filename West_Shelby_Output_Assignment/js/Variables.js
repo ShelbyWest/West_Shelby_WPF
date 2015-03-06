75fbfb21ca25133f516b1a6175cb066a28c5409f
Shelby West <sjwest@fullsail.edu>
2015-03-05 19:16:46 -0500

    Tested the Page and Added Spaces
    Tested the webpage and noticed the missing spaces. Added those spaces
    and tested it again

--- West_Shelby_Output_Assignment/js/Variables.js
+++ West_Shelby_Output_Assignment/js/Variables.js
@@ -11,8 +11,8 @@ var anyPets = 5; //Assigned number variable
 var favoriteMusic = "Bangtan Boys"; //Assigned string variable
 var olderSiblings = "Maria and Danielle"; //Assigned second string variable
 
-console.log("It is " + languageFluency + "that I am fluent in Korean."); //Adding and concatenating the first Boolean variable
-console.log("It is also " + wrestling + "that I have been a life long wrestling fan."); //Added and concatenated the second Boolean variable
-console.log("I have " + anyPets+ "dogs, all Boxers"); //Logging and concatenating the number variable
+console.log("It is " + languageFluency + " that I am fluent in Korean."); //Adding and concatenating the first Boolean variable
+console.log("It is also " + wrestling + " that I have been a life long wrestling fan."); //Added and concatenated the second Boolean variable
+console.log("I have " + anyPets+ " dogs, all Boxers"); //Logging and concatenating the number variable
 console.log("My favorite band is a Korean band called " + favoriteMusic + "."); //Logging and concatenating the first string variable
 console.log("I have two older sisters named, " + olderSiblings + "."); //Added and concatenated the second string variable
