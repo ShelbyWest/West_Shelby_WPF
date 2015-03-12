81f5668d1fbad82aee464588c0bac982acfe45e7
Shelby West <sjwest@fullsail.edu>
2015-03-12 16:52:40 -0400

    Learning Basic Expressions
    Using basic expressions to add 2 to 3

--- Git Repo/js/script.js
+++ Git Repo/js/script.js
@@ -1,13 +1,13 @@
 /*
 Shelby West
 Test JS file
-March 4, 2015
+March 12, 2015
  */
 
 //alert("Testing 1, 2, 3 ");
 
 
-console.log("Testing the console.log");
-
-var userInput = prompt("Type in your name:");
-console.log(userInput);
+var a = 2; //sets up variable a and defines it with 2
+var b;
+b = a + 3; //add 3 to a
+console.log(b);
