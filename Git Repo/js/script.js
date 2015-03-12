db26d7d9fc817f2e21d724de395c3e952d425680
Shelby West <sjwest@fullsail.edu>
2015-03-12 17:13:58 -0400

    Learning Modulo
    Using the modulo expression to learn the remainder of odd numbers.

--- Git Repo/js/script.js
+++ Git Repo/js/script.js
@@ -3,12 +3,8 @@ Shelby West
 Test JS file
 March 12, 2015
  */
-//Arithmetic Operators
+//Modulo
 //alert("Testing 1, 2, 3 ");
-//area of the triangle is half of the width times the height
-var width = 8;
-var height = 7;
-var area = width * height/2;
-console.log(area);
 
-
+var remainder = 211 % 2;
+console.log(remainder);
