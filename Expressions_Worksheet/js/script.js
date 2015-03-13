cb12b893c787d931b7bc99ae380e8d67e9e5b369
Shelby West <sjwest@fullsail.edu>
2015-03-12 21:23:53 -0400

    Finishing Expressions Worksheet
    Finishing the expression worksheet~

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -20,12 +20,26 @@ var peoplePresent = 12;
 var numberPizzas = 9;
 //Multiply number of slices by how many pizzas then divide by number of people
 var slicesPerPerson = slicesPerPizza * numberPizzas / peoplePresent;
-var slicesForSparky = slicesPerPizza * numberPizzas % peoplePresent; 
+var slicesForSparky = slicesPerPizza * numberPizzas % peoplePresent;
 //print out the output number of slices
 console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
 //print out the number of slices for Sparky
-console.log("Sparky got " + slicesForSparky + " slices of pizza.")
+console.log("Sparky got " + slicesForSparky + " slices of pizza.");
 
+//Discounts
+
+//Calculate the discounted price
+var orgPrice = 29.99;
+var discountPercentage = .30;
+var descItem = "Maxi Dress";
+var salesTaxPercent = .07;
+//multiply original price by discount percentage then multiply that by the sales tax
+var discount = orgPrice * discountPercentage;
+var discountPrice = orgPrice - discount;
+var taxes = discountPrice * salesTaxPercent;
+var taxPrice = discountPrice + taxes;
+//output the prices
+console.log("Your " + descItem + "was originally $" + orgPrice + " but after a " + discountPercentage + "% discount, it is now $" + discountPrice + "without tax, and $" + taxPrice + " with tax.");
 
 
 
