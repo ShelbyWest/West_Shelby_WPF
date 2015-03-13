74c79a01492512ca55f29b8843e496405efe7d17
Shelby West <sjwest@fullsail.edu>
2015-03-12 20:53:25 -0400

    Expressions Worksheet
    Finished the first two problems on the Expressions Worksheet.

---
+++ Expressions_Worksheet/js/script.js
@@ -0,0 +1,24 @@
+//Shelby West
+// March 12, 2015
+// Expressions Worksheet/**
+
+//Problem 1 Dog Years
+
+//Calculate Sparky's age in dog years
+var sparkysAge = 5;
+var dogYears = 7;
+//Dog age multiplied by 7 is the output
+var realAge = sparkysAge * dogYears;
+//print out the output age
+console.log("Sparky is 5 human years old which is " + realAge + " in dog years.");
+
+//Slice of Pie part 1
+
+//Calculate how many pieces of pizza each person can eat
+var slicesPerPizza = 8;
+var peoplePresent = 12;
+var numberPizzas = 9;
+//Multiply number of slices by how many pizzas then divide by number of people
+var slicesPerPerson = slicesPerPizza * numberPizzas / peoplePresent;
+//print out the output number of slices
+console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.")
