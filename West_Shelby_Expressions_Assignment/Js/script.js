0a7f893b1210570b7b119e09835a87bae13dc6a3
Shelby West <sjwest@fullsail.edu>
2015-03-12 22:20:56 -0400

    Adding the Arrays
    I forgot the array and added it in.

--- West_Shelby_Expressions_Assignment/Js/script.js
+++ West_Shelby_Expressions_Assignment/Js/script.js
@@ -1,7 +1,7 @@
 // Shelby West
 //March 12, 2015
 //Expressions Assignment
-// What is the area of a trapezoid?
+// What is the area of a trapezoid? Then add it to the given trapezoids.
 var topLength = prompt("We are finding the area of a trapezoid. \nPlease enter the length of the top."); //Prompting the user to enter the top length
 var height = prompt("Please enter the height of the trapezoid."); //prompting the user to enter the height
 var bottomLength = prompt("Please enter the bottom length.") //prompting the user to enter the bottom length
@@ -9,3 +9,8 @@ var bottomLength = prompt("Please enter the bottom length.") //prompting the use
 var area = 0.5 * (topLength + bottomLength) * height; //calculating the area
 
 console.log("The area of the trapezoid is " + area); //outputting the final result
+
+var otherTrapes = [ area, 4322, 3442]
+var allTrapes = area + otherTrapes[1] + otherTrapes[2];
+
+console.log("The area of all of the trapezoids is " + allTrapes);
