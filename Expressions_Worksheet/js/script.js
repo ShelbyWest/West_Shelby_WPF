890c2e38e3630d7633c560214fab83e465145705
Shelby West <sjwest@fullsail.edu>
2015-03-12 21:09:52 -0400

    Slices of Pie part 2
    Used modulo to find the number of slices of pizza Sparky got.

--- Expressions_Worksheet/js/script.js
+++ Expressions_Worksheet/js/script.js
@@ -12,13 +12,20 @@ var realAge = sparkysAge * dogYears;
 //print out the output age
 console.log("Sparky is 5 human years old which is " + realAge + " in dog years.");
 
-//Slice of Pie part 1
+//Slice of Pie part 2
 
-//Calculate how many pieces of pizza each person can eat
+//Calculate how many pieces of pizza Sparky can eat
 var slicesPerPizza = 8;
 var peoplePresent = 12;
 var numberPizzas = 9;
 //Multiply number of slices by how many pizzas then divide by number of people
 var slicesPerPerson = slicesPerPizza * numberPizzas / peoplePresent;
+var slicesForSparky = slicesPerPizza * numberPizzas % peoplePresent; 
 //print out the output number of slices
-console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.")
+console.log("Each person ate " + slicesPerPerson + " slices of pizza at the party.");
+//print out the number of slices for Sparky
+console.log("Sparky got " + slicesForSparky + " slices of pizza.")
+
+
+
+
