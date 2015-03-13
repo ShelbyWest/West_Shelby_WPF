a4c9fea40f6bb884fb26d456fae0e634462cb3d4
Shelby West <sjwest@fullsail.edu>
2015-03-12 21:49:02 -0400

    Starting over
    I didn’t realize volume was an example used.

--- West_Shelby_Expressions_Assignment/Js/script.js
+++ West_Shelby_Expressions_Assignment/Js/script.js
@@ -1,11 +1,6 @@
 // Shelby West
 //March 12, 2015
 //Expressions Assignment
-// Finding the volume of 2 identical cubes
-var length = prompt("We are finding the volume of 2 identical cubes. \nPlease enter the length of the pool.");
-var width = prompt("Please enter the width of the cubes.");
-var height = prompt("Please enter the height of the cubes.");
-
-
+//
 
 
