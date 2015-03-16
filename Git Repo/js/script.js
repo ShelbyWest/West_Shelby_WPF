bf9b80af3863ae84046b44bb302a06588a826b3e
Shelby West <sjwest@fullsail.edu>
2015-03-16 14:23:05 -0400

    Conditional Logic
    Following along with the conditional logic video.

--- Git Repo/js/script.js
+++ Git Repo/js/script.js
@@ -3,12 +3,14 @@ Shelby West
 Test JS file
 March 12, 2015
  */
-//Prompt and Alert
 //alert("Testing 1, 2, 3 ");
+//Conditional Logic
 
-var width = prompt("We are calculating the area of a rectangle. \nPlease enter the width.");
-var height = prompt("Please enter the height.");
-var area = width * height; //calculates that info
-//console.log(area); //prints it out to the console
-var result = "The area of your rectangle is " + area + " sq feet";
-alert(result);
+var oldEnough = false;
+
+//If the child is old enough, print to the console "You can ride!"
+if(oldEnough){
+    //code performed if condition is true
+    console.log("You can ride the coaster!");
+}
+console.log("what comes after");
