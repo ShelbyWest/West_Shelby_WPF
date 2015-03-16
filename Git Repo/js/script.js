23624405fdc12b413413c4e9949f434a28e0f830
Shelby West <sjwest@fullsail.edu>
2015-03-16 14:28:59 -0400

    Relational Expressions
    Following the relational expressions video.

--- Git Repo/js/script.js
+++ Git Repo/js/script.js
@@ -4,13 +4,14 @@ Test JS file
 March 12, 2015
  */
 //alert("Testing 1, 2, 3 ");
-//Conditional Logic
+//Conditional Logic - Relational Expressions
 
-var oldEnough = false;
+var kidHeight = 40;
+var minHeight = 48;
 
-//If the child is old enough, print to the console "You can ride!"
-if(oldEnough){
+//If the child is old enough, print to the console "You can ride!
+//If the kid is over 48 inches in height
+if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }
-console.log("what comes after");
