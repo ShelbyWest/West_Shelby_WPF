1d2548803c032112e35b41fc95a6129920f92684
Shelby West <sjwest@fullsail.edu>
2015-03-19 12:09:57 -0400

    Conditionals with an Expression
    Testing how to make conditionals with an expression.

--- Git Repo/js/script.js
+++ Git Repo/js/script.js
@@ -6,12 +6,12 @@ March 12, 2015
 //alert("Testing 1, 2, 3 ");
 //Conditional Logic - Relational Expressions
 
-var kidHeight = 40;
+var kidHeight = 47;
 var minHeight = 48;
-
+var sneakerLift = 2;
 //If the child is old enough, print to the console "You can ride!
 //If the kid is over 48 inches in height
-if(kidHeight > minHeight){
+if(kidHeight + sneakerLift > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
 }
