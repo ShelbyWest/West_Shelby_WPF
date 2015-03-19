f881f32b4acb543184e0f75f5d38453d08695bd0
Shelby West <sjwest@fullsail.edu>
2015-03-19 12:55:40 -0400

    Logical Operators
    Learning and practicing using the logical operators between two
    expressions.

--- Git Repo/js/script.js
+++ Git Repo/js/script.js
@@ -4,20 +4,15 @@ Test JS file
 March 12, 2015
  */
 //alert("Testing 1, 2, 3 ");
-//Conditional Logic - Relational Expressions
+//Conditional Logic - Logical operators
 
-var kidHeight = 52;
-var minHeight = 48;
-var wParentHeight = 45; //the height of the kid with the parent
-//If the child is old enough, print to the console "You can ride!
-//If the kid is over 48 inches in height
-if(kidHeight  > minHeight){
-    //code performed if condition is true
-    console.log("You can ride the coaster!");
-}else if(kidHeight > wParentHeight){
-    //you can ride with a parent present
-    console.log("You can ride, but only with a parent present.")
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
+//if the price of the phone is less than our budget AND if our paycheck is over 300
+if(iPhonePrice < budget || wonLottery){
+    console.log("We can buy the phone!");
 }else{
-    //sorry you have growing to do.
-    console.log("Sorry kid, You've got some growing to do first!");
+    console.log("No phone for you!!");
+
 }
