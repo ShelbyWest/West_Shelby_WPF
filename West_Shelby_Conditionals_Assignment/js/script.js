ef4cf04996fb1c6d6b42714da23a16a865cb4ff1
Shelby West <sjwest@fullsail.edu>
2015-03-19 17:06:19 -0400

    First Calculation
    Prompting the user for information on the dogs involved in this
    calculation and figuring out how much food to prepare.

--- West_Shelby_Conditionals_Assignment/js/script.js
+++ West_Shelby_Conditionals_Assignment/js/script.js
@@ -3,4 +3,26 @@
  * March 19, 2015
  * Conditionals Assignment
  */
+console.log("Let's find out how much food to give the animals!");
+//assigning a variable to the number of dogs the user enters.
+var numDogs = prompt("How many dogs do you have to feed?");
+
+if(numDogs === ""){
+    prompt("Please enter how many dogs there are.");
+    //Validating the user input
+}else {
+    console.log("There are " +numDogs+ " dogs to feed.");
+}
+numDogs = Number(numDogs);
+var dogWeight = confirm("Do the dogs weigh more than 10 lbs? Ok for Yes, Cancel for No.");
+
+
+if(dogWeight === true){
+    var numberOfScoops = numDogs * 2;
+    console.log("Please prepare " +numberOfScoops+ " scoops of dog food.");
+}else{
+    console.log("Please prepare " +numDogs+ " scoops of dog food.");
+}
+
+
 
