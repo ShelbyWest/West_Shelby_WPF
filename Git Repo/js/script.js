22cd7f4181793523a7b0539dec7d5810204efed5
Shelby West <sjwest@fullsail.edu>
2015-03-19 12:25:39 -0400

    Else If Expressions.
    Learning and practicing the else if statements.

--- Git Repo/js/script.js
+++ Git Repo/js/script.js
@@ -6,12 +6,18 @@ March 12, 2015
 //alert("Testing 1, 2, 3 ");
 //Conditional Logic - Relational Expressions
 
-var kidHeight = 47;
+var kidHeight = 52;
 var minHeight = 48;
-var sneakerLift = 2;
+var wParentHeight = 45; //the height of the kid with the parent
 //If the child is old enough, print to the console "You can ride!
 //If the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight  > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present.")
+}else{
+    //sorry you have growing to do.
+    console.log("Sorry kid, You've got some growing to do first!");
 }
