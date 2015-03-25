55df7928bd111cb60e8726ebe36ae12c06009470
Shelby West <sjwest@fullsail.edu>
2015-03-25 15:56:29 -0400

    Variable Scope
    Learning how the function works with variables of the same name.

--- Git Repo/js/script.js
+++ Git Repo/js/script.js
@@ -4,17 +4,15 @@ Test JS file
 March 12, 2015
  */
 //alert("Testing 1, 2, 3 ");
-//Functions - Function Execution
+//Functions - Variable Scope
 
-function outptMsa(){
-    console.log("Hello World");
-}
+var width = 5;
 
 function calcArea(){
     var width = 20;
     var height = 30;
     var area = width * height;
-    console.log(area);
+    //console.log(area);
 }
-
+console.log(width);
 calcArea();
