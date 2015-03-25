496caa52976c8c0fbc059743985e8e58f1611e9e
Shelby West <sjwest@fullsail.edu>
2015-03-25 17:14:12 -0400

    Anonymous Functions
    Learning how to use anonymous functions.

--- Git Repo/js/script.js
+++ Git Repo/js/script.js
@@ -4,16 +4,14 @@ Test JS file
 March 12, 2015
  */
 //alert("Testing 1, 2, 3 ");
-//Functions - Function vs Procedure
+//Functions - Anonymous Functions
 
-//this is a function
-function calcAreaF(width, height){
+var calcArea = function(width, height){//defining
+    //code the function runs
     var area = width * height;
     return area;
 }
 
-//This is a procedure
-function calcAreaP(width, height){
-    var area = width * height;
-    console.log(area); //no return
-}
+var a = calcArea(20, 30); //invoking
+
+console.log(a);
