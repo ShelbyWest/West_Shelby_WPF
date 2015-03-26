07d8794819f7004c0b6bf460908d782595c7e8b0
Shelby West <sjwest@fullsail.edu>
2015-03-26 16:27:23 -0400

    Loops
    Following along with the loops videos.

--- Git Repo/js/script.js
+++ Git Repo/js/script.js
@@ -4,14 +4,27 @@ Test JS file
 March 12, 2015
  */
 //alert("Testing 1, 2, 3 ");
-//Functions - Anonymous Functions
+//Loops
+console.log("While loops");
+var b = 50; //setups the index
 
-var calcArea = function(width, height){//defining
-    //code the function runs
-    var area = width * height;
-    return area;
+while (b > 0){
+    console.log(b + " kegs on the wall.");
+    b--; //increments or decrements the index
 }
 
-var a = calcArea(20, 30); //invoking
+console.log("Do while loops");
 
-console.log(a);
+var c = 10;
+
+do{
+    console.log(c + " kegs on the wall");
+    c--;
+}
+while(c > 0);
+
+console.log("For loops");
+
+for (var i = 10; i > 10; i--){
+    console.log(i + " kegs on the wall")
+}
