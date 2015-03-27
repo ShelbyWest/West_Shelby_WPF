217db4cae21ac5050f3d425621e678d8b2789852
Shelby West <sjwest@fullsail.edu>
2015-03-26 21:51:42 -0400

    Finished Commenting
    Finished the commenting on each line of the code.

--- West_Shelby_Functions_Assignment/js/script.js
+++ West_Shelby_Functions_Assignment/js/script.js
@@ -19,51 +19,51 @@ function promptValidation(lottery){ //creating the function to assure that the u
     return lottery; //returning the variable lottery
 }
 
-function powerballNumbers(min, max, num){
+function powerballNumbers(min, max, num){ //created the function to find the random powerball numbers
 
-    var lotteryNumberArray = [];
+    var lotteryNumberArray = []; //created the variable for the array of lottery numbers
 
-    for(var i = 0; i < num;  i++) {
-        var lotteryNumber = Math.round(Math.random() * (max - min) + min);
-        lotteryNumberArray[i] = lotteryNumber;
+    for(var i = 0; i < num;  i++) { //making the loop that assures the right amount of numbers are entered into the array
+        var lotteryNumber = Math.round(Math.random() * (max - min) + min); //using the math object to find a random group of numbers
+        lotteryNumberArray[i] = lotteryNumber; //making sure each number in the array is a random number
     }
 
-    return lotteryNumberArray;
+    return lotteryNumberArray; //returning the value of the array
 }
 
-function pbNumber(min, max){
+function pbNumber(min, max){ //created the function that will find the powerball number
 
-   var pb = Math.round(Math.random() * (max - min) + min);
+   var pb = Math.round(Math.random() * (max - min) + min); //using the math object to find a random powerball numbers
 
-    return pb
+    return pb //returning the value of the powerball number
 
 }
 
-function floridaLotteryNumbers(min, max, num){
+function floridaLotteryNumbers(min, max, num){ //created the function to find the Florida lottery numbers
 
-    var floridaArray = [];
+    var floridaArray = []; //created the variable for the array of lottery numbers
 
-    for(var b = 0; b < num; b++){
-        var floridaNumbers = Math.round(Math.random() * (max - min) + min);
-        floridaArray[b] = floridaNumbers;
+    for(var b = 0; b < num; b++){ //using the for loop to assure that the number of numbers in the array is correct
+        var floridaNumbers = Math.round(Math.random() * (max - min) + min); //using the math object to generate random numbers
+        floridaArray[b] = floridaNumbers; //making sure each number in the array is a random number
     }
-    return floridaArray;
+    return floridaArray; //returning the values in the array
 }
 
 
 //Main Code
 
-whichLottery = promptValidation(whichLottery);
+whichLottery = promptValidation(whichLottery); //code to run the validation prompt to make sure the user enters a lottery type
 
 
-if(whichLottery === "powerball"){
-    pbNumbers = powerballNumbers(1, 59, 5);
-    powerball = pbNumber(1, 35);
-    console.log("The powerball numbers are " +pbNumbers+ " with a powerball of " +powerball+ ".");
+if(whichLottery === "powerball"){ //using an if else statement to run the code for when the user enters 'powerball'
+    pbNumbers = powerballNumbers(1, 59, 5); //running the powerball number function to generate the random array of 5 numbers in the lottery between 1 and 59
+    powerball = pbNumber(1, 35); //running the pb number function to generate a random powerball number between 1 and 35
+    console.log("The powerball numbers are " +pbNumbers+ " with a powerball of " +powerball+ "."); //printing to the console the powerball lottery numbers and the powerball
 
-}else{
-    floridaLottery = floridaLotteryNumbers(1, 53, 6);
-    console.log("The Florida lottery numbers are" +floridaLottery+ ".");
+}else{ //if the user enters florida instead of powerball, this code will run
+    floridaLottery = floridaLotteryNumbers(1, 53, 6); //running the florida lottery function to generate the random array of 6 numbers between 1 and 53
+    console.log("The Florida lottery numbers are" +floridaLottery+ "."); //printing to the console the Florida lottery numbers.
 }
 
 
